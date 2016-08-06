19d53957913ab010b9b43f17eb5bb2a57fd90bd6
malkesh-m <malkesh.m@gmail.com>
2016-08-06 12:18:53 +0530

    Second commite

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Message = "Your application description page.";
             return View();
         }
 
